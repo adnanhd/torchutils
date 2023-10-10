3afcc832118afbbc5767553c66591af1f9bcb955
Adnan Harun Dogan <adnanharundogan@gmail.com>
2023-10-10 22:32:03 +0300

    Remove utils

--- torchutils/__init__.py
+++ torchutils/__init__.py
@@ -44,6 +44,6 @@ import torchutils.callbacks
 import torchutils.trainer
 # import torchutils.logging
 import torchutils.metrics
-import torchutils.utils
+# import torchutils.utils
 
-__version__ = torchutils.utils.Version('2.0.0')
+__version__ = '2.0.0'
