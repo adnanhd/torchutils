5bbb80f1e78cb378cd912deddb7ad6517713f230
Adnan Harun Dogan <adnanharundogan@gmail.com>
2023-10-13 15:12:34 +0300

    Fix valid_dataloader batch_size problem

--- torchutils/datasets/wrapper.py
+++ torchutils/datasets/wrapper.py
@@ -4,16 +4,16 @@ import torch
 
 class TrainerDataset(object):
     __slots__ = ('dataset', 'kwargs',)
+
     def __init__(self, dataset, **kwargs):
         self.dataset = dataset
         self.kwargs = kwargs
 
-    def dataloader(self, batch_size: int = None, train: bool = False, cuda: bool = False, **kwargs):
-        if batch_size is None:
-            batch_size = self.dataset.__len__()
+    def dataloader(self, train: bool = False, cuda: bool = False, **kwargs):
         for key, value in self.kwargs.items():
             kwargs.setdefault(key, value)
+        kwargs.setdefault('batch_size', self.dataset.__len__())
         kwargs.setdefault('shuffle', train)
         kwargs.setdefault('pin_memory', not cuda)
-        kwargs.setdefault('num_workers',0 if cuda else os.cpu_count())
-        return torch.utils.data.DataLoader(self.dataset, **kwargs, batch_size=batch_size)
+        kwargs.setdefault('num_workers', 0 if cuda else os.cpu_count())
+        return torch.utils.data.DataLoader(self.dataset, **kwargs)
