14f6194aadbb58fc10679f1231a9176226749c90
Adnan Harun Dogan <adnanharundogan@gmail.com>
2023-10-15 02:04:57 +0300

    Version 2.1

--- torchutils/callbacks/__init__.py
+++ torchutils/callbacks/__init__.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
-#import torchutils.callbacks.base
-#import torchutils.callbacks.early_stopping
-#import torchutils.callbacks.handler
-#import torchutils.callbacks.model_checkpoint
-
-#from .base import TrainerCallback, StopTrainingError, CallbackMethodNotImplementedError
-#from .handler import CallbackHandler
-
-#from .early_stopping import EarlyStopping
-#from .model_checkpoint import ModelCheckpoint
-#from .score_logger import ScoreLoggerCallback
-
-from .callback import TrainerCallback, StopTraining
+from .callback import TrainerCallback, StopTrainingException
 from .handler import CallbackHandler
+from .early_stopping import EarlyStopping
+from .model_checkpoint import ModelCheckpoint
--- torchutils/datasets/wrapper.py
+++ torchutils/datasets/wrapper.py
@@ -9,11 +9,14 @@ class TrainerDataset(object):
         self.dataset = dataset
         self.kwargs = kwargs
 
-    def dataloader(self, train: bool = False, cuda: bool = False, **kwargs):
+    def dataloader(self, train: bool = False, device: torch.device = 'cpu', **kwargs):
+        if isinstance(device, str):
+            device = torch.device(device)
+        assert isinstance(device, torch.device)
         for key, value in self.kwargs.items():
             kwargs.setdefault(key, value)
         kwargs.setdefault('batch_size', self.dataset.__len__())
         kwargs.setdefault('shuffle', train)
-        kwargs.setdefault('pin_memory', not cuda)
-        kwargs.setdefault('num_workers', 0 if cuda else os.cpu_count())
+        kwargs.setdefault('pin_memory', device.type != 'cuda')
+        kwargs.setdefault('num_workers', (device.type != 'cuda') * os.cpu_count())
         return torch.utils.data.DataLoader(self.dataset, **kwargs)
