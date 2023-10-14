925882b7a257075e88b4e70e137aa01ee00c38eb
Adnan Harun Dogan <adnanharundogan@gmail.com>
2023-10-15 02:03:40 +0300

    Update filters and fix bugs

--- torchutils/logging/filters.py
+++ torchutils/logging/filters.py
@@ -6,22 +6,55 @@ def scoreFilter(record):
                                 "VALID_STEP", "EVAL_RUN", "EVAL_STEP")
 
 
-def scoreFilterRun(record):
-    return record.levelname in ("TRAIN_EPOCH", "EVAL_RUN", "VALID_RUN")
-
-
-def scoreFilterStep(record):
-    return record.levelname in ("VALID_STEP", "TRAIN_STEP", "EVAL_STEP")
-
-
-class scoreTrainStepFilter(logging.Filter):
-    def __init__(self, n):
+class scoreFilterRun(logging.Filter):
+    def __init__(self, training_epoch_per_filtering=0, validation_run_per_filtering=0):
+        assert training_epoch_per_filtering >= 0
+        assert validation_run_per_filtering >= 0
         super().__init__()
-        self.n = n
-        self.counter = 0
+
+        self.train_threshold = training_epoch_per_filtering
+        self.valid_threshold = validation_run_per_filtering
+        self.train_cnt = 0
+        self.valid_cnt = 0
+
+    def filter(self, record):
+        if record.levelname == 'TRAIN_EPOCH':
+            if self.train_threshold == 0:
+                return True
+            else:
+                self.train_cnt = (self.train_cnt + 1) % self.train_threshold
+                return self.train_cnt == 0
+        if record.levelname == 'VALID_RUN':
+            if self.valid_threshold == 0:
+                return True
+            else:
+                self.valid_cnt = (self.valid_cnt + 1) % self.valid_threshold
+                return self.valid_cnt == 0
+        return True
+
+
+class scoreFilterStep(logging.Filter):
+    def __init__(self, training_step_per_filtering=0, validation_step_per_filtering=0):
+        assert training_step_per_filtering >= 0
+        assert validation_step_per_filtering >= 0
+        super().__init__()
+
+        self.train_threshold = training_step_per_filtering
+        self.valid_threshold = validation_step_per_filtering
+        self.train_cnt = 0
+        self.valid_cnt = 0
 
     def filter(self, record):
         if record.levelname == 'TRAIN_STEP':
-            self.counter = (self.counter + 1) % self.n
-            return self.counter == 0
+            if self.train_threshold == 0:
+                return True
+            else:
+                self.train_cnt = (self.train_cnt + 1) % self.train_threshold
+                return self.train_cnt == 0
+        if record.levelname == 'VALID_STEP':
+            if self.valid_threshold == 0:
+                return True
+            else:
+                self.valid_cnt = (self.valid_cnt + 1) % self.valid_threshold
+                return self.valid_cnt == 0
         return True
