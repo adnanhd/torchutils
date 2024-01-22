99a2574e09d772da0b61c6d9a42ca1e6b986d2a0
adnanhd <adnanharundogan@gmail.com>
2024-01-22 18:01:43 +0100

    Incorporating repo https://github.com/szymonmaszke/torchdatasets into datasets

---
+++ torchutils/datasets/.old/__init__.py
@@ -0,0 +1,2 @@
+from .wrapper import TrainerDataset
+from .dataset import NumpyDataset
--- torchutils/datasets/__init__.py
+++ torchutils/datasets/__init__.py
@@ -1,2 +1,7 @@
-from .wrapper import TrainerDataset
-from .dataset import NumpyDataset
+# this submodule is taken from https://github.com/szymonmaszke/torchdatasets
+# Copyright © 2024 Szymon Maszke
+
+from . import cachers, datasets, maps, modifiers, samplers
+from .datasets import Dataset, Iterable
+from .wrappers import DataLoaderWrapper
+__version__ = '0.2.0'
